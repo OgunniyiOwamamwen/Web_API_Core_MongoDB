0e338a71305ee5c27df4a14f34a8875598ba9395
OgunniyiOwamamwen <ogunniyiowamamwen@gmail.com>
2019-10-05 17:47:57 +0200

    Add project files.

---
+++ Web_API_Core_MongoDB/Dal/BookstoreDatabaseSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_API_Core_MongoDB.Repository;
+
+namespace Web_API_Core_MongoDB.Dal
+{
+    public class BookstoreDatabaseSettings : IBookstoreDatabaseSettings
+    {
+        public string BooksCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ Web_API_Core_MongoDB/Service/BookService .cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Web_API_Core_MongoDB.Repository;
+using MongoDB.Driver;
+using Web_API_Core_MongoDB.Models;
+
+namespace Web_API_Core_MongoDB.Service
+{
+    public class BookService
+    {
+        //Add a CRUD operations service
+
+        private readonly IMongoCollection<Book> _books;
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString); //My ConnectionString to Database
+            var database = client.GetDatabase(settings.DatabaseName); //My Connection to DatabaseName
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() => _books.Find(book => true).ToList();
+
+        public Book Get(string id) => _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn) =>
+            _books.ReplaceOne(book => book.Id == id, bookIn);
+
+        public void Remove(Book bookIn) =>
+            _books.DeleteOne(book => book.Id == bookIn.Id);
+
+        public void Remove(string id) =>
+            _books.DeleteOne(book => book.Id == id);
+
+    }
+}
